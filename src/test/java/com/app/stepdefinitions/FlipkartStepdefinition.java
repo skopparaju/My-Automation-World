0f4d9bc2ebde379e07357cf63e3a8fd5e9bf0309
skopparaju <srinivas.kopparaju@gmail.com>
2025-07-25 20:42:43 +0530

    Code fix

--- src/test/java/com/app/stepdefinitions/FlipkartStepdefinition.java
+++ src/test/java/com/app/stepdefinitions/FlipkartStepdefinition.java
@@ -1,89 +1,56 @@
 package com.app.stepdefinitions;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-
-
+import com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter;
 import com.flipkart.webpages.FlipkartHomePage;
-import com.vimalselvam.cucumber.listener.Reporter;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+public class FlipkartStepdefinition {
 
+    FlipkartHomePage FHP = new FlipkartHomePage();
 
-public class FlipkartStepdefinition  {
-	
-	
-	
-	FlipkartHomePage FHP = new FlipkartHomePage();
-	
-	
+    String actual = "";
+    String expected = "";
+    List<String> actual_List;
+    List<String> expected_List;
 
-	
-	String actual = "";
-	String expected = "";
-	List<String> actual_List;
-	
-	List<String> expected_List;
-	
-	@Then("^Validate user should able to navigates to mobiles page$")
-	public void validate_user_should_able_to_navigates_to_mobiles_page() throws Exception {
-		Thread.sleep(1000);
-		FHP.clickClose();
+    @Then("^Validate user should able to navigates to mobiles page$")
+    public void validate_user_should_able_to_navigates_to_mobiles_page() throws Exception {
+        Thread.sleep(1000);
+        FHP.clickMobiles();
+        actual = FHP.getTitle();
+        expected = "Mobile Phones Online at Best Prices in India1";
+        assertEquals("Mismatch in page title ",expected, actual);
+        ExtentCucumberAdapter.addTestStepLog("Successfully navigated to mobiles page");
+    }
 
-       FHP.clickMobiles();
-		
-		 actual = FHP.getTitle();
-		
-		Reporter.addStepLog("The current title of the page is "+actual);
-		
-		 expected = "Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!";
-		
-		Reporter.addStepLog("The  title should display is "+expected);
-		assertEquals(expected,actual);
-	}
-	
-	@When("^User Search for a product$")
-	public void User_for_product() throws Exception {
-		Thread.sleep(1000);
-		FHP.clickClose();
-		FHP.enterPoductName("Iphone");
-		FHP.clickSearch();
-		
-		
-		Thread.sleep(1000);
-		
-	}
+    @When("^User Search for a product$")
+    public void User_for_product() throws Exception {
+        Thread.sleep(1000);
+        FHP.clickClose();
+        FHP.enterPoductName("Iphone");
+        FHP.clickSearch();
+        Thread.sleep(1000);
+    }
 
-	
-	@Then("^Validate Search result lable should display$")
-	public void Validate_Search_result_lable_should_display() {
-		
-		expected_List = new ArrayList<String>();
-		 actual_List = FHP.getSortOptions();
-		
-		Reporter.addStepLog("The sort options for the product  are "+actual_List);
-		String values ="Relevance,Popularity,Price -- Low to High,Price -- High to Low,Newest First";
-		
-		String[] options = values.split(",");
-		
-		for(String option:options) {
-			expected_List.add(option);	
-		} 	
-	
+    @Then("^Validate Search result lable should display$")
+    public void Validate_Search_result_lable_should_display() {
+        expected_List = new ArrayList<>();
+        actual_List = FHP.getSortOptions();
+
+        String values = "Relevance,Popularity,Price -- Low to High,Price -- High to Low,Newest First";
+        String[] options = values.split(",");
+
+        for (String option : options) {
+            expected_List.add(option);
+        }
 
 
-		
-		Reporter.addStepLog("The sort options for the product  should contain "+expected_List);
-		
-		assertEquals(expected_List,actual_List);
-
-
-		
-	}
+        assertEquals(expected_List, actual_List);
+    }
 }
--- src/test/resources/Features/Flipkart.feature
+++ src/test/resources/Features/Flipkart.feature
@@ -6,15 +6,10 @@ Scenario: Validating able to navigate mobiles page
 Then Validate user should able to navigates to mobiles page
  
 
-
-
 @ValidateSortOptions
 Scenario Outline: Validating sort options
 When User Search for a product
 Then Validate Search result lable should display
- 
-
-
 Examples:
 |sheet|dsID|
 |Flipkart|ds001|
